b77dd370b93c588ff3b90b58e0691f7488224331
MiuraMao <m_miura06@outlook.com>
2023-02-14 21:23:00 +0900

    List2.2

---
+++ javaapplet/HelloWorldApplet.java
@@ -0,0 +1,9 @@
+package javaapplet;
+import java.awt.*;
+import java.applet.*;
+
+public class HelloWorldApplet  {
+    public void paint(Graphics g) {
+        g.drawString("Hello World",20, 20);
+    }
+}
