bbc270db111ff065edc9d6894fbe697424d2d36c
MiuraMao <m_miura06@outlook.com>
2023-03-05 19:30:00 +0900

    List2.13

--- ServletA.java
+++ ServletA.java
@@ -1,5 +1,5 @@
 //サーブレットコンテキスト
-//複数のサーブレットコンテキストで属性名を付けて共有（文字列など）
+//複数のサーブレットで属性名を付けて共有（文字列など）
 //サーブレットAの処理
 ServletContext application = this.getServletConfig().getServletContext();
 application.setAttribute ("SharedString", new String("This is a shared message"));
